61722b2deac0330e4eaf5126721b22ee36ce52e0
jfloff <jfloff@gmail.com>
2012-07-01 19:39:28 +0100

    9.1.3 -- All green

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,4 +21,13 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
+  end
 end
