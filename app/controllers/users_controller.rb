5e5edf6c76bce707993d967dfaedeb47ca5c2650
jfloff <jfloff@gmail.com>
2012-07-02 15:57:02 +0100

    9.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,12 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :signed_in_user, only: [:index, :edit, :update]
+  before_filter :correct_user,   only: [:edit, :update]
 
   def new
     @user = User.new
   end
 
   def show
-    @user = User.find(params[:id])
-  end
-
-  def edit
     @user = User.find(params[:id])
   end
 
@@ -24,8 +21,10 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       flash[:success] = "Profile updated"
       sign_in @user
@@ -35,9 +34,18 @@ class UsersController < ApplicationController
     end
   end
 
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   private
 
     def signed_in_user
       redirect_to signin_path, notice: "Please sign in." unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
     end
 end
