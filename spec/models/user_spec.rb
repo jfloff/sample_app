75d1d39551253eeee503cd8e5a1e213fb0106649
jfloff <jfloff@gmail.com>
2012-07-02 17:32:25 +0100

    9.6.1

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -118,4 +118,12 @@ describe User do
     before { @user.save }
     its(:remember_token) { should_not be_blank }
   end
+
+  describe "accessible attributes" do
+    it "should not allow access to admin" do
+      expect do
+        User.new(name: @user.name, email: @user.email, password: @user.password, password_confirmation: @user.password_confirmation, admin: true )
+      end.should raise_error(ActiveModel::MassAssignmentSecurity::Error)
+    end
+  end
 end
